6b69d6e3658f4b1fa60df1792c8ee58ebb6f13e7
SilviaCaraballo <silviacaraballofernandez@gmail.com>
2024-12-16 23:45:20 +0100

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Silvia Caraballo Fernandez on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
